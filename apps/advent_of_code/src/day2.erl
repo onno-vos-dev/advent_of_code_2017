8d035d3981dea7cab6498e94c284a1d727d9aaaf
onno.vos.dev <onno-vos-dev@users.noreply.github.com>
2017-12-02 15:39:18 +0100

    Slightly changed implementation for 2b

--- apps/advent_of_code/src/day2.erl
+++ apps/advent_of_code/src/day2.erl
@@ -3,42 +3,38 @@
 -export([ two/0
         ]).
 
--export([input/0]).
-
+%% My solution for 2b is pretty damn nasty. Will try to clean up later tonight
 two() ->
   L = input(),
   A = solve_a(L),
   B = solve_b(L),
-  {{day1, lists:sum(A)}, {day2, lists:sum(B)}}.
+  {{day1, A}, {day2, B}}.
 
 solve_a(List) ->
-  lists:map(fun(Row) ->
-                [L|T] = lists:sort(Row),
-                lists:last(T) - L
-            end, List).
+  lists:sum(
+    lists:map(fun(Row) ->
+                  [L|T] = lists:sort(Row),
+                  lists:last(T) - L
+              end, List)).
 
-%% Here be dragons
 solve_b(List) ->
-  lists:map(
-    fun(Row) ->
-        [{RowRes}] = lists:flatten(
-                     lists:map(
-                       fun(Val1) ->
-                           lists:filtermap(
-                             fun(Val2) ->
-                                 if Val1 == Val2 ->
-                                     false;
-                                    true ->
-                                     case Val1 rem Val2 of
-                                       0 ->
-                                         {true, {Val1 div Val2}};
-                                       _ -> false
-                                     end
-                                 end
-                             end, Row)
-                       end, Row)),
-          RowRes
-    end, List).
+  lists:foldl(
+    fun(Row, Acc) ->
+        lists:sum(
+          lists:flatten(
+            lists:map(
+              fun(Val1) ->
+                  lists:filtermap(
+                    fun(Val2) ->
+                        case Val1 == Val2 orelse (Val1 rem Val2 =/= 0) of
+                          true -> false;
+                          _ -> {true, Acc + (Val1 div Val2)}
+                        end
+                    end, Row)
+              end, Row)
+           )
+         )
+    end, 0, List).
 
 input() ->
   {ok, Bin} = file:read_file(code:priv_dir(advent_of_code) ++ "/day2.txt"),
@@ -46,7 +42,6 @@ input() ->
   lists:map(fun(Row) ->
                 lists:map(fun(Rv) -> list_to_integer(Rv) end, Row)
             end, R).
-
 
 %%%_* Emacs ====================================================================
 %%% Local Variables:
