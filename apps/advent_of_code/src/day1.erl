2a28f796f608ddb137bc04466d3c8c3e64ba0a07
onno.vos.dev <onno-vos-dev@users.noreply.github.com>
2017-12-02 02:28:06 +0100

    For funz add parallel day 1a and 1b

--- apps/advent_of_code/src/day1.erl
+++ apps/advent_of_code/src/day1.erl
@@ -1,23 +1,36 @@
 -module(day1).
 
 -export([ one/0
+        , parallel/0
         ]).
 
--define(LF(X), lists:flatten(X)).
 -define(REX_OPTS, [{capture, first, list}, global]).
 
 one() ->
-  {ok, Bin} = file:read_file(code:priv_dir(advent_of_code) ++ "/day1.txt"),
-  {match, L} = re:run(binary_to_list(Bin), "([0-9])", ?REX_OPTS),
+  L = input(),
   {_, A} = captcha(L, 1),
   {_, B} = captcha(L, length(L) div 2),
   {{day1, A}, {day2, B}}.
+
+parallel() ->
+  L = input(),
+  A = fun() -> captcha(L, 1) end,
+  B = fun() -> captcha(L, length(L) div 2) end,
+  Self = self(),
+  Pid1 = spawn_link(fun() -> Self ! {self(), {day1, A()}} end),
+  Pid2 = spawn_link(fun() -> Self ! {self(), {day2, B()}} end),
+  [ receive {Pid, R} -> R end || Pid <- [Pid1, Pid2] ].
+
+input() ->
+  {ok, Bin} = file:read_file(code:priv_dir(advent_of_code) ++ "/day1.txt"),
+  {match, L} = re:run(binary_to_list(Bin), "([0-9])", ?REX_OPTS),
+  L.
 
 captcha(List, Nth) ->
   LL = List ++ List,
   lists:foldl(fun(L, {N, Sum}) ->
                   case L =:= lists:nth(N + Nth, LL) of
-                    true -> {N + 1, Sum + list_to_integer(?LF(L))};
+                    true -> {N + 1, Sum + list_to_integer(lists:flatten(L))};
                     false -> {N + 1, Sum}
                   end
               end, {1, 0}, List).
